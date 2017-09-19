94747c37ce6f32e7cddebb0e8c86a5d762a65b16
Agney Menon <agney@dexlock.com>
2017-09-19 18:35:45 +0530

    General Layout:

--- src/Content.jsx
+++ src/Content.jsx
@@ -7,13 +7,14 @@ function Content() {
       <Card.Group>
         <Card fluid raised className="single-card">
           <Card.Content>
-            <Card.Header>
-              matthew
+            <Card.Header as="div">
+              <span>matthew</span>
             </Card.Header>
+            <p className="small-font">San Fransisco</p>
           </Card.Content>
           <Image src="https://source.unsplash.com/random/400x400" />
           <Card.Content>
-            <div>
+            <div className="action-container">
               <span><i className="icon ion-ios-heart-outline" /></span>
               <span><i className="icon ion-ios-chatbubble-outline" /></span>
             </div>
@@ -27,6 +28,9 @@ function Content() {
               </strong>
               <span> like this</span>
             </p>
+            <Card.Meta className="small-font">
+              4 HOURS AGO
+            </Card.Meta>
           </Card.Content>
         </Card>
       </Card.Group>
--- src/HeaderContainer.jsx
+++ src/HeaderContainer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Header, Segment, Container } from 'semantic-ui-react';
+import { Header, Container, Search } from 'semantic-ui-react';
 
 function HeaderContainer() {
   return (
     <Container fluid as="header">
-      <Header as='div' size='huge' dividing>
-        <Header textAlign='center' as='h1'>INSTATON</Header>
+      <Header as='div' size='huge' dividing className="header-container">
+        <div className="left-header">
+          <i className="icon ion-social-instagram" />
+          <Header textAlign='center' as='h1'>INSTATON</Header>
+        </div>
+        <div className="center-header">
+          <Search size="mini" />
+        </div>
+        <div className="right-header">
+          <i className="icon ion-android-compass" />
+          <i className="icon ion-ios-heart" />
+          <i className="icon ion-android-person" />
+        </div>
       </Header>
     </Container>
   );
